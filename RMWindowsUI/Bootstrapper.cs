75cb645077b3e2b9703fce990135e857e52e2bc6
Tin Nguyen <tinnguyen2002@gmail.com>
2024-01-29 23:39:46 -0500

    Added Caliburn to packages, Setup ShellViewModel and ShellView
    Created base classes for ShellViewModel and ShellView
    Created Bootstrapper to setup caliburn and setup ShellViewModel as startup page. Added Bootstrapper to App.xaml resource dictionary

---
+++ RMWindowsUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using RMWindowsUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMWindowsUI
+{
+    // setup caliburn.micro
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // on start up launch ShellViewModel as base view
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
