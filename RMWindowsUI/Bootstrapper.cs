8c3df06595009ab47cc3d3d4303ae892d5110c31
Tin Nguyen <tinnguyen2002@gmail.com>
2024-01-30 23:08:43 -0500

    Update WPF Project with MVVM
    Adds our WPF ui project and configures it with Caliburn Micro for MVVM framework. Aldded simple container dependency injection system

--- RMWindowsUI/Bootstrapper.cs
+++ RMWindowsUI/Bootstrapper.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using RMWindowsUI.ViewModels;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,54 @@ namespace RMWindowsUI
     // setup caliburn.micro
     public class Bootstrapper : BootstrapperBase
     {
+        // dependency injection container handles instantiation of all classes
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                // be careful about using singletons unless you know you should, aren't great on memory usage
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // handle view models that connect to views
+            // reflection, relfection is slow, usually dont use it
+            // Configure only runs once at the startup, so no need to worry about tiny performance hit
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    // similar to Singleton format, first arg could be interface of viewModel and then viewModelType,
+                    // might need to change later to do unit testing
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             // on start up launch ShellViewModel as base view
             DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
