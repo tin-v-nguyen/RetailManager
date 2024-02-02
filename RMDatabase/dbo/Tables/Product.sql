377278cd636eb9d5a878731edd8c596be188fa0c
Tin Nguyen <tinnguyen2002@gmail.com>
2024-02-02 16:01:05 -0500

    Register DB Tables
    The tables for basic register/sale system

---
+++ RMDatabase/dbo/Tables/Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    --stored in disk different, small performance penalty when using MAX
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [RetailPrice] MONEY NOT NULL,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
---
+++ RMDatabase/dbo/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	--auth user id comes from authentication database, entity framework, which is a 128 char GUID
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+	--EF uses 256
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate() 
+)
