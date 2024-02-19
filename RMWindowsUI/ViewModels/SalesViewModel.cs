472087fd906f4cd15ed063100d579a7d16b8d404
Tin Nguyen <tinnguyen2002@gmail.com>
2024-02-18 22:25:04 -0500

    Wired up Add To Cart Button
    Added SelectedProduct property to SalesViewModel
    
    Updated Cart BindingList type to Cart Item Model
    
    Created Cart Item Model to store Product, Quantity of that product in cart, and what to display in the cart
    
    Default item quantity to 1
    
    Wired up subtotal to calculate based on cart
    
    Updated CanAddToCart to check if there is enough in stock for selected product
    
    In AddToCart method, used lambda function to find if selected prod exists, updates item quantity if found by using hack of removing and readding, else create new CartItem and add to cart, update Quantity in stock locally, reset Quantity field to 1

--- RMWindowsUI/ViewModels/SalesViewModel.cs
+++ RMWindowsUI/ViewModels/SalesViewModel.cs
@@ -43,19 +43,33 @@ namespace RMWindowsUI.ViewModels
             }
 		}
 
-        private BindingList<ProductModel> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<ProductModel> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
             {
-                _products = value;
+                _cart = value;
                 NotifyOfPropertyChange(() => Cart);
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
         // using int means that quantity has to be typed as a number in the textbox
 		public int ItemQuantity
 		{
@@ -64,14 +78,25 @@ namespace RMWindowsUI.ViewModels
 			{ 
 				_itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
 		}
 
-
-        public string Subtotal
+        private string _subTotal;
+        public string SubTotal
         {
             // replace with calculation
-            get { return "$0.00"; }
+            get 
+            {
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += item.Product.RetailPrice * item.QuantityInCart;
+                }
+
+                return subTotal.ToString("C");
+            }
+            
         }
 
         public string Tax
@@ -97,6 +122,10 @@ namespace RMWindowsUI.ViewModels
 
                 // make sure an item is selected
 				// make sure there is an item quantity
+                if (ItemQuantity >= 1 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
 
                 return output;
@@ -105,8 +134,29 @@ namespace RMWindowsUI.ViewModels
         }
         public void AddToCart()
 		{
-
-		}
+            // compare to find exact same object lambda expression
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK should be a better way of refreshing cart
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            } else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity,
+                };
+                Cart.Add(item);
+            }
+            
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Cart);
+        }
 
         public bool CanRemoveFromCart
         {
@@ -124,7 +174,7 @@ namespace RMWindowsUI.ViewModels
         }
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
