dca18662f705c93c3c53718e37e9cf84d31fdccc
Tin Nguyen <tinnguyen2002@gmail.com>
2024-02-20 23:10:28 -0500

    Changes ShellViewModel to use IoC.Get to get an instance of LoginViewModel, instead of passing in the container

--- RMWindowsUI/ViewModels/ShellViewModel.cs
+++ RMWindowsUI/ViewModels/ShellViewModel.cs
@@ -15,21 +15,20 @@ namespace RMWindowsUI.ViewModels
         
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
+        
 
         // dont need to save loginVM "state"
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             // constructor injection to pass in an instance loginVM and store it in _loginVM
             
             _events = events;
             _salesVM = salesVM;
-            _container = container;
-
+            
             _events.SubscribeOnPublishedThread(this);
 
             // get new loginvm and replaces it each time, clear sensitive information
-            ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
         }
 
         public async Task HandleAsync(LogOnEventModel message, CancellationToken cancellationToken)
