bb3656428fe5514cd0230ab8dff8919f72749229
Tin Nguyen <tinnguyen2002@gmail.com>
2024-03-01 13:53:18 -0500

    Added Controller function and route "users/admin/getallusers/
    Gets all users, their information, and roles

--- RMDataManager/Controllers/UserController.cs
+++ RMDataManager/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RMDataManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
@@ -20,5 +23,39 @@ namespace RMDataManager.Controllers
             return data.GetUserByID(userId).First();
         }
 
+        [Authorize (Roles = "Admin")]
+        [HttpGet]
+        [Route("Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var users = userManager.Users.ToList();
+                var roles = context.Roles.ToList();
+
+                foreach (var user in users)
+                {
+                    ApplicationUserModel userModel = new ApplicationUserModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email,
+                    };
+
+                    foreach (var r in user.Roles)
+                    {
+                        userModel.Roles.Add(r.RoleId, roles.Where(x => x.Id == r.RoleId).First().Name);
+                    }
+
+                    output.Add(userModel);
+                }
+            }
+
+            return output;
+            
+        }
     }
 }
